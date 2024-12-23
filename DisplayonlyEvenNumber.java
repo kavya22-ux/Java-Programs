9bd89846daeebc211bc8e091413682e8152a2b1c
kavya22-ux <79582583+kavya22-ux@users.noreply.github.com>
2024-12-23 06:06:58 +0000

    Add files via upload

---
+++ DisplayonlyEvenNumber.java
@@ -0,0 +1,26 @@
+package basicPrograms;
+
+import java.util.Scanner;
+
+public class DisplayonlyEvenNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int number;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the number");
+		
+		number = sc.nextInt();
+		
+		for(int i=0; i < number; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		sc.close();
+
+	}
+
+}
